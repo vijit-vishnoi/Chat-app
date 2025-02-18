f1ca311d52c15ad4e303370a3b1304afeee80506
vijit-vishnoi <vishnoivijit@gamil.com>
2025-02-19 03:21:26 +0530

    Basic socket setup done

---
+++ public/script.js
@@ -0,0 +1,13 @@
+var socket=io();
+
+let btn=document.getElementById('btn');
+btn.onclick=function exec(){
+    socket.emit('from_client');
+}
+socket.on('from_server',()=>{
+    console.log("Collected a new event from server");
+    const div=document.createElement('div');
+    div.innerText='New event from server';
+    console.log(div);
+    document.body.appendChild(div);
+})
