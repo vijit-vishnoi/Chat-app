95caa40b34eca05d27f05a278cb4903d79aa6865
vijit-vishnoi <vishnoivijit@gamil.com>
2025-02-21 20:35:37 +0530

    Added chat logic

---
+++ config/database-config.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose');
+
+const connect=async()=>{
+    await mongoose.connect("mongodb://localhost/chatapp");
+
+}
+
+module.exports=connect;
--- index.js
+++ index.js
@@ -2,25 +2,57 @@ const express=require('express');
 const http=require('http');
 const socketio=require('socket.io');
 
+const Chat=require('./models/chat.js')
+const connect=require('./config/database-config.js')
 const app=express();
 const server=http.createServer(app);
 const io=socketio(server);
 
 io.on('connection',(socket)=>{
+    socket.on('join_room',(data)=>{
+        console.log("joining a room",data.roomid)
+        socket.join(data.roomid,function(){
+            console.log("joined a room");
+        });
+    });
 
-    socket.on('from_client',()=>{
-        console.log("event coming from client");
+    socket.on('msg_send',async(data)=>{
+        console.log(data);
+        const chat=await Chat.create({
+            roomId: data.roomid,
+            user: data.user,
+            content: data.msg
+        })
+        socket.to(data.roomid).emit('msg_rcvd',data);
+    });
+    socket.on('typing',(data)=>{
+        socket.broadcast.to(data.roomid).emit('someone_typing',{
+            user: data.user
+        });
     })
-
-    setInterval(()=>{
-        socket.emit('from_server');
-    },2000);
 });
 
 
-
+app.set('view engine','ejs');
 app.use('/',express.static(__dirname+'/public'));
-server.listen(3000,()=>{
+
+app.get('/chat/:roomid',async(req,res)=>{
+    const chats=await Chat.find({
+        roomId: req.params.roomid
+    }).select('content user');
+    console.log(chats);
+    res.render('index',{
+        name: 'Vijit',
+        id: req.params.roomid,
+        chats:chats
+    });
+    
+})
+
+
+server.listen(3000,async()=>{
     console.log('Server started');
+    await connect();
+    console.log("mongo db connected")
 })
 
